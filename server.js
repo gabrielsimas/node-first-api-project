30fbc3bc550da0001573735736a33d053d4a819a
Luis Gabriel N. Simas <luis.simas_paros@prestador.globo>
2021-05-30 19:27:40 -0300

    Aula 04 - Normalizando a porta

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const express = require('express');
 const app = express();
 
 //TODO: Utilizar node env
-const port = 3000;
+const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
 const server = http.createServer(app);
@@ -24,3 +24,17 @@ app.use('/', route);
 
 server.listen(port);
 console.log(`API rodando na porta ${port}`);
+
+function normalizePort(val) {
+  const port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    return val;
+  }
+
+  if (port >= 0) {
+    return port;
+  }
+
+  return false;
+}
