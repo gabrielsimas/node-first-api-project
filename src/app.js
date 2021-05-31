879cb6bb3d4b8cc6548dbedd19990657686863a0
Luis Gabriel N. Simas <luis.simas_paros@prestador.globo>
2021-05-31 06:48:49 -0300

    Aula 14 - Mongoose

--- src/app.js
+++ src/app.js
@@ -2,8 +2,19 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// Conecta ao Banco
+//TODO: Alterar os dados para variáveis de ambiente protegidas por senha
+mongoose.connect(
+  'mongodb+srv://gabrielsimas:J4n3c4554n1@nodeapicluster.bsxoi.gcp.mongodb.net/api-com-node',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+);
 
 // Carrega as Rotas
 const indexRoute = require('./routes/index.js');
